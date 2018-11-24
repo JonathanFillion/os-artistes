35c27b2781a7cd3db73af05457d86bd027e83ee3
Jonathan Fillion <fillion.jonathan@gmail.com>
2018-11-24 11:06:22 -0500

    Changement des booleans d'etat a des etat enum

--- src/concoursDeDessin/Artiste.java
+++ src/concoursDeDessin/Artiste.java
@@ -3,10 +3,13 @@ package concoursDeDessin;
 public class Artiste extends Thread {
 	boolean aUneFeuille = false;
 	boolean ilNeRestePlusDeFeuilles = false;
+	private enum Etat {RIEN,POSSEDE_FEUIL, POSSEDE_FEUIL_ET_CRAYONS, DESSIN_FAIT}
+	private Etat etat = Etat.RIEN;
 	int feuilleId;
 	// Crayon Rouge, Bleu, Jaune, Vert
 	boolean[] crayonsPossede = { false, false, false, false };
 	int id;
+	int testNombreDeCrayons = 0;
 	Semaphore pileDeFeuilleSemaphore;
 	Semaphore remettreDessinSemaphore;
 	// Semaphore Crayon Rouge, Bleu, Jaune, Vert
@@ -22,57 +25,59 @@ public class Artiste extends Thread {
 	public void run() {
 
 		while (true) {
-			if (!this.aUneFeuille) {
+			if (etat == Etat.RIEN) {
 				pileDeFeuilleSemaphore.P();
 				if (PileDeFeuilles.nombreDeFeuilles != 0) {
 					feuilleId = PileDeFeuilles.nombreDeFeuilles;
 					PileDeFeuilles.nombreDeFeuilles--;
-					this.aUneFeuille = true;
+					etat = Etat.POSSEDE_FEUIL;
 					System.out.println(Thread.currentThread().getName() + " prend " + feuilleId);
 				} else {
 					ilNeRestePlusDeFeuilles = true;
 				}
 				pileDeFeuilleSemaphore.V();
 			}
-			while (true && !possedeTousLesCrayons() && this.aUneFeuille) {
+			while (etat == Etat.POSSEDE_FEUIL) {
 				if (!crayonsPossede[0]) {
 					crayonsSemaphores[0].P();
 					crayonsPossede[0] = true;
-
+					testNombreDeCrayons++;
 				}
 				if (!crayonsPossede[1]) {
 					crayonsSemaphores[1].P();
 					crayonsPossede[1] = true;
-
+					testNombreDeCrayons++;
 				}
 				if (!crayonsPossede[2]) {
 					crayonsSemaphores[2].P();
 					crayonsPossede[2] = true;
-
+					testNombreDeCrayons++;
 				}
 				if (!crayonsPossede[3]) {
 					crayonsSemaphores[3].P();
 					crayonsPossede[3] = true;
-
+					testNombreDeCrayons++;
 				}
 
 				if (possedeTousLesCrayons()) {
-					System.out.println(Thread.currentThread().getName() + " a tous ses crayons");
-					break;
+					//System.out.println(Thread.currentThread().getName() + " a tous ses crayons. Nombre = " + testNombreDeCrayons);
+					etat = Etat.POSSEDE_FEUIL_ET_CRAYONS;
 				}
 			}
 			
-			if (possedeTousLesCrayons()) {
+			if (etat == Etat.POSSEDE_FEUIL_ET_CRAYONS) {
+				System.out.println(Thread.currentThread().getName() + " fait un dessin sur feuille " + this.feuilleId);
+				etat = Etat.DESSIN_FAIT;
 				for (int i = 0; i < 4; i++) {
 					crayonsPossede[i] = false;
 					crayonsSemaphores[i].V();
+					testNombreDeCrayons--;
 				}
-				System.out.println(Thread.currentThread().getName() + " a relaché tous ses crayons");
 			}
 			
-			if (this.aUneFeuille) {
+			if (etat == Etat.DESSIN_FAIT) {
 				remettreDessinSemaphore.P();
-				this.aUneFeuille = false;
+				etat = Etat.RIEN;
 				PileDeDessin.nombreDessinRemis++;
 				System.out.println(Thread.currentThread().getName() + " remet " + feuilleId);
 				remettreDessinSemaphore.V();
